c421a81ddf6fdbc3944a16b5ab2a5b52750c89dc
saeki <87316126+gin-gali@users.noreply.github.com>
2022-01-11 21:13:39 +0900

    addTodos wip

---
+++ my-profile/src/components/addTodos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form>
+      <input type="text" placeholder="Todo">
+    </form>
+    <input type="submit" value="Add" @click="addTodo">
+  </div>
+</template>
+
+<script setup>
+function addTodo() {
+  
+}
+</script>
---
+++ my-profile/src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <div>
+      <form>
+        <input type="submit" @click="addTodo">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const message = "タスクを入力してね"
+
+</script>
